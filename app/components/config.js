6c04d10b62e261af7efc23dde0ba81a5353fa02d
mikexbcn <mikemulderx@gmail.com>
2026-05-09 15:48:58 +0200

    Cambio de fondo y carpeta public

--- app/components/config.js
+++ app/components/config.js
@@ -7,9 +7,16 @@ export const getFlag = (sigla) => {
 };
 
 export const bgImages = {
-  landing: "https://images.unsplash.com/photo-1551958219-acbc608c6377?q=80&w=2070",
+  landing: "/images/marcos-moraes-rFehnP5wN4Q-unsplash.jpg",
+  dentro: "/images/michael-lee-6J7eIvNwttQ-unsplash.jpg",
+  predictions: "https://images.unsplash.com/photo-1574629810360-7efbbe195018?q=80&w=2093",
+  honor: "https://images.unsplash.com/photo-1518091044754-94b1fd56977c?q=80&w=1974",
+  ranking: "https://images.unsplash.com/photo-1508098682722-e99c43a406b2?q=80&w=2070",
+
+/*  landing: "https://images.unsplash.com/photo-1551958219-acbc608c6377?q=80&w=2070",
   dentro: "https://images.unsplash.com/photo-1574629810360-7efbbe195018?q=80&w=2093",
   predictions: "https://images.unsplash.com/photo-1574629810360-7efbbe195018?q=80&w=2093",
   honor: "https://images.unsplash.com/photo-1518091044754-94b1fd56977c?q=80&w=1974",
   ranking: "https://images.unsplash.com/photo-1508098682722-e99c43a406b2?q=80&w=2070",
+*/
 };
